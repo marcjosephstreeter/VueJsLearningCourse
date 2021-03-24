7944f022351a743fe7421e2b5b6d743519adb632
Marc Joseph Streeter <marcjosephstreeter@gmail.com>
2021-03-24 18:20:22 -0500

    No need to call a function now

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: { 
+    fullname() {
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'Schwarzmuller'
+    }
+  },
   methods: {
     outputFullname() {
       if(this.name === ''){
